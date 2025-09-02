c1d89d408f0c8f6847f5538c9b57b5ec5c73f597
matty199905 <matty199905@gmail.com>
2025-09-02 17:58:23 -0300

    integracion de mercadopago

--- src/app/api/create-preference/route.ts
+++ src/app/api/create-preference/route.ts
@@ -8,7 +8,7 @@ export async function POST(req: Request) {
     const response = await fetch("https://api.mercadopago.com/checkout/preferences", {
       method: "POST",
       headers: {
-        Authorization: `Bearer ${process.env.MP_ACCESS_TOKE}`,
+        Authorization: `Bearer ${process.env.MP_ACCESS_TOKEN}`,
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
